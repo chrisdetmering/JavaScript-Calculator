b73052ab83e320eb12d9284328e79b203efc4141
nickpaolino <nick_paolino@yahoo.com>
2021-04-21 16:17:16 -0400

    switch statements

--- app.js
+++ app.js
@@ -20,14 +20,53 @@ function getCalculatorValues() {
 }
 getCalculatorValues()
 
-function getCalculatorOperators () {
-    const operators = document.querySelectorAll('.operator');
-
+function getMemoryOperators () {
+    const operators = document.querySelectorAll('.memory-operator');
     for (operator of operators) {
         operator.addEventListener('click', (e) => {
            const getOperators = e.target.id;
-           console.log(getOperators)
+           switch (getOperators) {
+               case 'clear':
+                   alert('clear')
+                   break;
+               case 'memory-recall':
+                   alert('memory-recall')
+                   break;    
+                case 'add-memory':
+                    alert('add-memory ')
+                    break;
+                case 'remove-memory':
+                    alert('remove-memory')
+                    break;    
+           }
         })
     } 
 }
-getCalculatorOperators()
+getMemoryOperators()
+
+function getMathOperators () {
+    const operators = document.querySelectorAll('.math-operator');
+    for (operator of operators) {
+        operator.addEventListener('click', (e) => {
+           const getOperators = e.target.id;
+           switch (getOperators) {
+               case 'add':
+                   alert('add')
+                   break;
+               case 'subtract':
+                   alert('subtract')
+                   break;    
+                case 'multiply':
+                    alert('multiply')
+                    break;
+                case 'divide':
+                    alert('divide')
+                    break;    
+                case 'equals':
+                    alert('equals')
+                    break;        
+           }
+        })
+    } 
+}
+getMathOperators()
