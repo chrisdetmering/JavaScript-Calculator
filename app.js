ccace70d4454b3af184af5ba75489f06b7346ef6
froggy <chrisdetmering@gmail.com>
2021-04-27 13:12:20 -0700

    Andy refactor completed

--- app.js
+++ app.js
@@ -6,127 +6,111 @@ let operator = '';
 let memory = '';
 
 function handleDecimal () {
-    if (!operator && firstOperand.indexOf('.') === -1) {
+    if (!operator && !isDecimal(firstOperand)) {
         firstOperand += '.';
         calcDisplay.textContent = firstOperand;
-    } else if (secondOperand.indexOf('.') === -1 && operator){
+        return; 
+    } 
+    
+    if (!isDecimal(secondOperand) && operator){
         secondOperand += '.';
         calcDisplay.textContent = secondOperand;
     }
 }
 
-document.getElementById('.').addEventListener('click', handleDecimal);
+document.getElementById('.')
+.addEventListener('click', handleDecimal);
 
-function setOperands() {
     
-    document.querySelectorAll('.numbers')
-    .forEach(numberButton =>  {
-        numberButton.addEventListener('click', () => {
-            const number = numberButton.id
-            if(!operator) {
-                firstOperand += number;
-                calcDisplay.textContent = firstOperand
-            }
-            if (firstOperand && operator) {
-                secondOperand += number;
-                calcDisplay.textContent = secondOperand;
-            }
-        })
+document.querySelectorAll('.numbers')
+.forEach(numberButton =>  {
+    numberButton.addEventListener('click', () => {
+        const number = numberButton.id
+        if(!operator) {
+            firstOperand += number;
+            calcDisplay.textContent = firstOperand
+        }
+        if (firstOperand && operator) {
+            secondOperand += number;
+            calcDisplay.textContent = secondOperand;
+        }
     })
-}
-setOperands()
-
-
-function setMathOperators() {
-    document.querySelectorAll('.operator')
-    .forEach(operatorButton => {
-    operatorButton.addEventListener('click', (e) => {
+})
+ 
+document.querySelectorAll('.operator')
+.forEach(operatorButton => {
+operatorButton.addEventListener('click', (e) => {
         const selectedOperator = e.target.id;
-     
+    
         if (firstOperand && !operator) {
-        operator = selectedOperator;
-        }
-        else if (secondOperand) {
-            const result = eval(calculate());
-            firstOperand = result;
             operator = selectedOperator;
+            return; 
+        }
+
+        if (firstOperand && operator && secondOperand){ 
+            answer = calculate();
+            firstOperand = answer
             secondOperand = '';
-            calcDisplay.textContent = firstOperand;   
-            }
-         
-        })
-    }) 
-}
-setMathOperators()
-
-function setMemoryOperators() {
-    document.querySelectorAll('.memory-operator')
-    .forEach(memoryButton => {
-    memoryButton.addEventListener('click', (e) => {
-        const memoryOperator = e.target.id;
-        operator = memoryOperator;
-        
-        switch(operator) { 
-            case 'add-memory':
-                memory = Number(memory) + Number(calcDisplay.textContent);
-                calcDisplay.textContent = '0';
-                operator = '';
-                firstOperand = '';
-                break;
-            case 'subtract-memory':
-                memory -= calcDisplay.textContent;
-                calcDisplay.textContent = '0';
-                operator = '';
-                firstOperand = '';
-                break;
-            case 'memory-recall':
-                if (!memory) {
-                    calcDisplay.textContent = '0'
-                } else {
-                    calcDisplay.textContent = memory;
-                    firstOperand = calcDisplay.textContent;
-                    operator = '';
-                    break;
-                }      
-            }
-        })
-    })
-}
-setMemoryOperators()
-
-function setEqualsButton(){
-    document.querySelector('.equals') 
-    .addEventListener('click', (e) => {
-        const equalsButton = e.target.id;
-        if (equalsButton) {
-            if (!operator) {
-            equalsButton.disabled = true;
-            } else if (firstOperand && secondOperand){ 
-                answer = eval(calculate())
-                calcDisplay.textContent = answer;
-                firstOperand = calcDisplay.textContent;
-                secondOperand = '';
-                operator = '';
-            }
+            operator = selectedOperator;
+            calcDisplay.textContent = firstOperand;
+            
         }
     })
-}
+}) 
 
-setEqualsButton()
-
-function setClearButton() {
-    document.querySelector('.clear')
-    .addEventListener('click', (e) => {
-        const clearButton = e.target.id;
-        if (clearButton) {
+document.querySelectorAll('.memory-operator')
+.forEach(memoryButton => {
+memoryButton.addEventListener('click', (e) => {
+    const memoryOperator = e.target.id;
+    operator = memoryOperator;
+    
+    switch(operator) { 
+        case 'add-memory':
+            memory = Number(memory) + Number(calcDisplay.textContent);
             calcDisplay.textContent = '0';
-            firstOperand = '';
-            secondOperand = '';
             operator = '';
+            firstOperand = '';
+            break;
+        case 'subtract-memory':
+            memory -= calcDisplay.textContent;
+            calcDisplay.textContent = '0';
+            operator = '';
+            firstOperand = '';
+            break;
+        case 'memory-recall':
+            if (!memory) {
+                calcDisplay.textContent = '0'
+            } else {
+                calcDisplay.textContent = memory;
+                firstOperand = calcDisplay.textContent;
+                operator = '';
+                break;
+            }      
         }
-    }) 
-}
-setClearButton()
+    })
+})
+
+
+
+document.querySelector('.equals') 
+.addEventListener('click', (e) => {
+    if (firstOperand && operator && secondOperand){ 
+        answer = calculate();
+        firstOperand = answer;
+        secondOperand = '';
+        operator = '';
+        calcDisplay.textContent = firstOperand;
+    }
+})
+
+document.querySelector('.clear')
+.addEventListener('click', () => {
+    calcDisplay.textContent = '0';
+    firstOperand = '';
+    secondOperand = '';
+    operator = '';
+}) 
+
 
 function calculate() {
     switch (operator) {
@@ -136,20 +120,26 @@ function calculate() {
             return subtract();       
         case 'multiply':
             return multiply();
-        case 'divide':Calculator
+        case 'divide':
             return divide();                    
     }
 }
 
 function add() {
-    return `${Number(firstOperand)} + ${Number(secondOperand)}`;
+    return `${Number(firstOperand) + Number(secondOperand)}`;
 }
 function subtract() {
-    return `${Number(firstOperand)} - ${Number(secondOperand)}`;
+    return `${Number(firstOperand) - Number(secondOperand)}`;
 }
 function multiply() {
-    return `${Number(firstOperand)} * ${Number(secondOperand)}`;
+    return `${Number(firstOperand) * Number(secondOperand)}`;
 }
 function divide() {
-    return `${Number(firstOperand)} / ${Number(secondOperand)}`;
+    return `${Number(firstOperand) / Number(secondOperand)}`;
+}
+
+
+//UTILS
+function isDecimal(string) {
+    return string.indexOf('.') > -1;
 }
